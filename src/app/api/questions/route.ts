2d819238ab41a2fa2b7d1b98d076ceb7587ed499
Ikhlasul Amal Rivel <amalrivel@gmail.com>
2025-12-31 03:35:16 +0700

    feat: update 0.1.0 app, update error while build app

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -1,20 +1,20 @@
-// import { NextResponse } from "next/server";
-// import { getQuestions } from "@/lib/questions";
-// import { requireAuthedEmail } from "@/lib/auth";
+import { NextResponse } from "next/server";
+import { getQuestions } from "@/lib/questions";
+import { requireAuthedEmail } from "@/lib/auth";
 
-// export async function GET() {
-//   try {
-//     await requireAuthedEmail();
+export async function GET() {
+  try {
+    await requireAuthedEmail();
 
-//     const questions = await getQuestions();
-//     return NextResponse.json({ questions });
-//   } catch (e: any) {
-//     // kalau redirect terjadi di API, lebih baik balikin 401
-//     const msg = e?.message ?? "";
-//     if (msg.includes("NEXT_REDIRECT")) {
-//       return NextResponse.json({ error: "unauthorized" }, { status: 401 });
-//     }
+    const questions = await getQuestions();
+    return NextResponse.json({ questions });
+  } catch (e: any) {
+    // kalau redirect terjadi di API, lebih baik balikin 401
+    const msg = e?.message ?? "";
+    if (msg.includes("NEXT_REDIRECT")) {
+      return NextResponse.json({ error: "unauthorized" }, { status: 401 });
+    }
 
-//     return NextResponse.json({ error: msg || "Unknown error" }, { status: 500 });
-//   }
-// }
+    return NextResponse.json({ error: msg || "Unknown error" }, { status: 500 });
+  }
+}
